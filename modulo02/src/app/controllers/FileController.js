c227692daa4e1819165dcf0311c97bac59b96249
Paulo Junior <psjuniorx8@gmail.com>
2019-11-19 20:16:18 -0300

    Inclusão dos arquivos do Multer

---
+++ modulo02/src/app/controllers/FileController.js
@@ -0,0 +1,12 @@
+import File from '../models/file';
+
+class FileController {
+  async store(request, response) {
+    const { originalname: name, filename: path } = request.file;
+
+    const file = await File.create({ name, path });
+    return response.json(file);
+  }
+}
+
+export default new FileController();
